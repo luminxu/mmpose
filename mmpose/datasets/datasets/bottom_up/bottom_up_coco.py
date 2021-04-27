79eae51c99cca797d91db52442bd8b21248bc970
luminxu <luminxu@link.cuhk.edu.hk>
2021-04-27 15:00:38 +0800

    .

--- mmpose/datasets/datasets/bottom_up/bottom_up_coco.py
+++ mmpose/datasets/datasets/bottom_up/bottom_up_coco.py
@@ -280,7 +280,7 @@ class BottomUpCocoDataset(BottomUpBaseDataset):
         valid_kpts = []
         for img in kpts.keys():
             img_kpts = kpts[img]
-            nms = soft_oks_nms if self.soft_nms else oks_nms
+            nms = soft_oks_nms
             keep = nms(list(img_kpts), oks_thr, sigmas=self.sigmas)
             valid_kpts.append([img_kpts[_keep] for _keep in keep])
 
